7a7ad50b8d9879a7ea7b4524d5c764fa1ab1e1df
Chris Johnson <studywork@Chriss-MacBook-Air.local>
2021-05-14 17:14:07 +1000

    Added routers and changed the folder structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
       <h1>Oh Hey!</h1>
-      .
-      .
-      .
       <h1>Hey! How's it going?</h1>
-    </div>
+        <Switch>
+          <Route exact path="/" component={Homepage}/>;
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
