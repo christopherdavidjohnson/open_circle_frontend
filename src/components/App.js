9bd54e02022cbcacf89116af3a0b36ac6275965f
Chris Johnson <studywork@Chriss-MacBook-Air.local>
2021-05-17 10:12:15 +1000

    pages update

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,29 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { HashRouter as HashRouter, Switch, Route, Link} from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Profile from "./pages/Profile";
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <div className="App">
+
+        <ul>
+          <li>
+            <Link to="/homepage">Home</Link>
+          </li>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+        </ul>
+
+
         <Switch>
           <Route path="/homepage" component={Homepage}/>;
           <Route path="/profile" component={Profile}/>;
         </Switch>
       </div>
-    </Router>
+    </HashRouter>
   );
 }
 
