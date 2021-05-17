0254ca34eb13244665ac42af6142c854872c4952
faithsylvia1 <you@example.com>
2021-05-17 14:26:51 +1000

    changes to App.js for user login and add navbar div

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { HashRouter as HashRouter, Switch, Route, Link} from "react-router-dom";
+import axios from 'axios'
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Login from "./pages/Login";
@@ -9,55 +10,89 @@ import Messages from "./pages/Messages";
 import Feed from "./pages/Feed";
 import './stylesheets/App.css';
 
-function App() {
-  return (
-    <HashRouter>
-      <div className="App">
+class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+      user: {}
+    };
+  }
 
-          <div className="nav">
-            <Link to="/homepage">Home</Link>
-          </div>
+  handleLogin = (data) => {
+    this.setState({
+      isLoggedIn: true,
+      user: data.user
+    })
+  }
+  handleLogout = () => {
+    this.setState({
+    isLoggedIn: false,
+    user: {}
+    })
+  }
 
-          <div className="nav">
-            <Link to="/login">Login</Link>
-          </div>
+  loginStatus = () => {
+    axios.get('http://localhost:3001/logged_in',
+   {withCredentials: true})
+    .then(response => {
+      if (response.data.logged_in) {
+        this.handleLogin(response)
+      } else {
+        this.handleLogout()
+      }
+    })
+    .catch(error => console.log('api errors:', error))
+  }
 
-          <div className="nav">
-            <Link to="/signup">Signup</Link>
-          </div>
+  render() {
+    return (
+      <HashRouter>
+        <div className="App">
+          <div className="navbar">
+            <div className="nav">
+              <Link to="/homepage">Home</Link>
+            </div>
 
-          <div className="nav">
-            <Link to="/profile">Profile</Link>
-          </div>
+            <div className="nav">
+              <Link to="/login">Login</Link>
+            </div>
 
-          <div className="nav">
-            <Link to="/circles">Circles</Link>
-          </div>
+            <div className="nav">
+              <Link to="/signup">Signup</Link>
+            </div>
 
-          <div className="nav">
-            <Link to="/messages">Messages</Link>
-          </div>
+            <div className="nav">
+              <Link to="/profile">Profile</Link>
+            </div>
 
-          <div className="nav">
-            <Link to="/feed">Feed</Link>
-          </div>
+            <div className="nav">
+              <Link to="/circles">Circles</Link>
+            </div>
 
+            <div className="nav">
+              <Link to="/messages">Messages</Link>
+            </div>
 
-        <Switch>
+            <div className="nav">
+              <Link to="/feed">Feed</Link>
+            </div>
+          </div> 
+          <Switch>
 
-          <Route path="/login" component={Login}/>;
-          <Route path="/signup" component={Signup}/>;
-          <Route path="/profile" component={Profile}/>;
-          <Route path="/circles" component={Circles}/>;
-          <Route path="/messages" component={Messages}/>;
-          <Route path="/feed" component={Feed}/>;
-          <Route path="/" component={Home}/>;
+            <Route path="/login" component={Login}/>;
+            <Route path="/signup" component={Signup}/>;
+            <Route path="/profile" component={Profile}/>;
+            <Route path="/circles" component={Circles}/>;
+            <Route path="/messages" component={Messages}/>;
+            <Route path="/feed" component={Feed}/>;
+            <Route path="/" component={Home}/>;
 
-        </Switch>
-      </div>
-    </HashRouter>
-  );
+          </Switch>
+        </div>
+      </HashRouter>
+    );
+  }
 }
-
 export default App;
