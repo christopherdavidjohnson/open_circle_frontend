7ea0db0dfdfd7f650972fead4649e9eee2b94d63
Kimberley Osborn <kimberleyfaith@Kimberleys-MacBook-Pro.local>
2021-05-19 16:16:32 +1000

    message update

--- src/components/pages/Messages.js
+++ src/components/pages/Messages.js
@@ -4,31 +4,30 @@ import '../stylesheets/Messages.css';
 
 const SERVER_URL = "https://open-circle-server.herokuapp.com/messages.json";
 
-// class Messages extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       messages: []
-//     };
-//   }
-//
-//   render() {
-//     return (
-//       <div>
-//         <h1>Write your message</h1>
-//       </div>
-//     )
-//   }
-//
-// }
-//
-//   class MessageForm extends Component {
-//     render() {
-//       return (
-//
-//       );
-//     }
-//   }
-//
-//
-// export default Messages;
+class Messages extends Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: []
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Write your message</h1>
+      </div>
+    )
+  }
+
+}
+
+  class MessageForm extends Component {
+    render() {
+      return (
+
+      );
+    }
+  }
+
+ export default Messages;
